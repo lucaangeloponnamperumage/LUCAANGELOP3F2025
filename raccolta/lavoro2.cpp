229f91f15e7e9c8f07b3a2bfd5f2a96b960dad35
Luca Angelo Ponnamperumage <ponnamperumage.lucaangelo.2023@itisgalileiroma.it>
2026-01-03 01:28:14 +0100

    esercizi

---
+++ raccolta/lavoro2.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{int n1,n2;
+cout << "Inserisci 2 numeri"<< endl;
+cin >> n1;
+cin >> n2;
+if(n1==n2) {cout << n1 << n2 << endl;}
+else if(n1>n2) {cout << n2 << n1 << endl;}
+else if(n1<n2) {cout << n1 << n2 << endl;};
+
+
+
+
+
+
+
+
+
+
+}
---
+++ raccolta/lavoro34.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{double n;
+string risp,
+cout << "Scrivi un numero"<< endl;
+cin >> n;
+cout << "Vuoi trasformarlo in minuti o secondi?"<< endl:
+cin >> risp;
+if(risp == "secondi") {cout << n*3600 << endl;}
+if(risp == "minuti") {cout << n*60 << endl;};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
